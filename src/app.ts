e0b22f3ba00dd885b1392adcd3e534a81ac8cd8d
HP <beeboyabiodun111@gmail.com>
2026-04-12 17:48:12 +0100

    Fix

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,8 @@ import promoRoutes from './routes/promo.routes';
 const app: Express = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 app.use('/api/auth', authRoutes);
